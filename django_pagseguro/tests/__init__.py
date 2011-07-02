1274cc251bd96918871c4bf8370eca25096e4a24
Fábio Cerqueira <cerqueirasfabio@gmail.com>
2011-07-02 01:49:58 -0300

    Adicionado teste para verificação das contantes obrigatórias

--- django_pagseguro/tests/__init__.py
+++ django_pagseguro/tests/__init__.py
@@ -1,4 +1,4 @@
-from django.test import TestCase
+from django.utils import unittest
 
 import django_pagseguro 
 
@@ -6,4 +6,10 @@ from django_pagseguro.tests.signals import *
 from django_pagseguro.tests.pagseguro import *
 from django_pagseguro.tests.views import *
 
+class ConfiguracoesDaApp(unittest.TestCase):
 
+    def test_cosntantes_no_settings(self):
+        from django.conf import settings
+        self.assertTrue(hasattr(settings, 'PAGSEGURO_EMAIL_COBRANCA'))
+        self.assertTrue(hasattr(settings, 'PAGSEGURO_TOKEN'))
+        self.assertTrue(hasattr(settings, 'PAGSEGURO_URL_FINAL'))
