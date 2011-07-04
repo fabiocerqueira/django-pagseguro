93862c4bd857366384a64406f18f2436c47b9a4f
Fábio Cerqueira <cerqueirasfabio@gmail.com>
2011-07-04 10:17:51 -0300

    Adicionado testes de validação da view

---
+++ django_pagseguro/tests/views.py
@@ -0,0 +1,31 @@
+from django.utils import unittest
+from django.test.client import Client
+from django.conf import settings
+
+class PagSeguroRetornoViewTest(unittest.TestCase):
+    def test_view_retorno_com_get(self):
+        c = Client()
+        res = c.get('/pagseguro/retorno/')
+        self.assertEquals(res.status_code, 302)
+        url_final_valida = res['location'].endswith(settings.PAGSEGURO_URL_FINAL)
+        self.assertTrue(url_final_valida)
+
+    def test_view_retorno_post_ok(self):
+        from django_pagseguro import pagseguro
+        def mock_req_pagseguro(params):
+            return 'VERIFICADO'
+        pagseguro._req_pagseguro = mock_req_pagseguro
+        c = Client()
+        dados = {'StatusTransacao':'Aprovado', 'Referencia':42}
+        res = c.post('/pagseguro/retorno/', dados)
+        self.assertEquals(res.content, 'OK')
+
+    def test_retorno_post_falha(self):
+        from django_pagseguro import pagseguro
+        def mock_req_pagseguro(params):
+            return 'FALSO'
+        pagseguro._req_pagseguro = mock_req_pagseguro
+        c = Client()
+        dados = {'StatusTransacao':'Aprovado', 'Referencia':42}
+        res = c.post('/pagseguro/retorno/', dados)
+        self.assertEquals(res.content, 'FALHA')
